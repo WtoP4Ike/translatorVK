b2a010c3a9a0f69408c42634cb168b701971df1f
WtoP4Ike <118076364+WtoP4Ike@users.noreply.github.com>
2023-04-07 21:56:16 +0300

    Create trconfig.py

---
+++ trconfig.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# # # # Copyright (C) 2023 Vladimir Zakharov
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # This file is part of TranslatorVK
+#
+# TranslatorVK is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# any later version.
+#
+# TranslatorVK is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#General information about the bot
+name="Bot translator for Vkontakte"
+version = 1.0
+author = "Wtop4ike"
+#Setting up the bot
+token="" #bot token
+log_chat="2000000003" #peer_id of the chat for logs
+lang="" #ru/en - Main language
+#messages
